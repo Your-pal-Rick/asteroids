6e8e2a2e0287f111e858b5e852cf0b3e23f85eb1
Your-pal-Rick <r.gameau@mailfence.com>
2025-03-13 23:42:05 +1030

    move game loop to game.py

--- game.py
+++ game.py
@@ -1,1 +1,71 @@
-# Imports here
+""" 
+Asteroids Game - Game Module
+
+This module contains the main game loop and initialisation logic for the Asteroids.
+It handles rendering, game updates, collision detection, and player input. """
+
+# Standard library imports
+import sys
+
+# Third-party imports
+import pygame
+
+# Local application imports
+from constants import *
+from player import Player
+from shot import Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+class Game:
+    """ Class Docstring """
+
+    def __init__(self):
+        """ Initialise the game """
+        
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.dt = 0
+        
+        self.asteroids = pygame.sprite.Group()
+        self.shots = pygame.sprite.Group()
+        self.updatable = pygame.sprite.Group()
+        self.drawable = pygame.sprite.Group()
+
+        Player.containers = (self.updatable, self.drawable)
+        Asteroid.containers = (self.updatable, self.drawable, self.asteroids)
+        AsteroidField.containers = (self.updatable)
+        Shot.containers = (self.shots, self.updatable, self.drawable)
+
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.my_player = Player(PLAYER_SPAWN_POSITION[0], PLAYER_SPAWN_POSITION[1])
+        self.my_asteroidfield = AsteroidField()
+
+    def run(self):
+        """ Temporarily transferring unrefactored main loop in here """
+        
+        while (True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            
+            self.updatable.update(self.dt)
+
+            for asteroid in self.asteroids:
+                if asteroid.is_colliding(self.my_player):
+                    sys.exit("Game over!")
+
+            for asteroid in self.asteroids:
+                for shot in self.shots:
+                    if asteroid.is_colliding(shot):
+                        shot.kill()
+                        asteroid.split()
+
+            self.screen.fill(BLACK)
+
+            for member in self.drawable:
+                member.draw(self.screen)
+
+            pygame.display.flip()
+
+            self.dt = self.clock.tick(FRAMES_PER_SECOND) / 1000 # to milliseconds
--- main.py
+++ main.py
@@ -1,75 +1,31 @@
 """
 Asteroids Game - Main Module
 
-This module contains the main game loop and initialisation logic for the Asteroids.
-It handles rendering, game updates, collision detection, and player input.
+Based on the boot.dev (https://boot.dev) project, Build Asteroids Using Python,
+which I am refactoring as practise with OOP principles in mind.
+
+This is the entry point, which initialises and runs the game using the Game class.
 
 Usage:
     Run this file directly to start the game:
     $ python main.py
+
+Dependencies:
+    - Pygame (https://pygame.org)
+    - Python 3.x
+
+Controls:
+    - WASD:         Move ship
+    - Space bar:    Fire
+    - ESC:          Quit game (not implemented yet)
 """
 
-# Standard library imports
-import sys
-
-# Third-party imports
-import pygame
-
 # Local application imports
-from constants import *
-from player import Player
-from shot import Shot
-from asteroid import Asteroid
-from asteroidfield import AsteroidField
-
-def initialise_game():
-    """Set up game environment"""
-    pygame.init()
-
+from game import Game
 
 def main():
-    initialise_game()
-    clock = pygame.time.Clock()
-    dt = 0
-    asteroids = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-
-    Player.containers = (updatable, drawable)
-    Asteroid.containers = (updatable, drawable, asteroids)
-    AsteroidField.containers = (updatable)
-    Shot.containers = (shots, updatable, drawable)
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    my_player = Player(PLAYER_SPAWN_POSITION[0], PLAYER_SPAWN_POSITION[1])
-    my_asteroidfield = AsteroidField()
-
-    while (True):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        
-        updatable.update(dt)
-
-        for asteroid in asteroids:
-            if asteroid.is_colliding(my_player):
-                sys.exit("Game over!")
-
-        for asteroid in asteroids:
-            for shot in shots:
-                if asteroid.is_colliding(shot):
-                    shot.kill()
-                    asteroid.split()
-
-        screen.fill(BLACK)
-
-        for member in drawable:
-            member.draw(screen)
-
-        pygame.display.flip()
-
-        dt = clock.tick(FRAMES_PER_SECOND) / 1000 # to milliseconds
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
