e851e4e6dfa2db972a3cf65426962bf9c8b96b51
Your-pal-Rick <r.gameau@mailfence.com>
2025-03-14 00:14:30 +1030

    merge modules, decompose game initialisation, add some documentation

--- game.py
+++ game.py
@@ -2,7 +2,8 @@
 Asteroids Game - Game Module
 
 This module contains the main game loop and initialisation logic for the Asteroids.
-It handles rendering, game updates, collision detection, and player input. """
+It handles rendering, game updates, collision detection, and player input.
+"""
 
 # Standard library imports
 import sys
@@ -12,32 +13,50 @@ import pygame
 
 # Local application imports
 from constants import *
-from player import Player
-from shot import Shot
-from asteroid import Asteroid
-from asteroidfield import AsteroidField
+from player import Player, Shot
+from asteroid import Asteroid, AsteroidField
 
 class Game:
-    """ Class Docstring """
-
+    """ Game class - Initialises game and handles
+    entirety of game loop. Will soon be neater. """
     def __init__(self):
-        """ Initialise the game """
-        
+        self._init_game()
+
+    def _init_game(self):
+        """Initialise the game"""
+        self._init_game_state()
+        self._init_pygame()
+        self._create_sprite_groups()
+        self._assign_containers()
+        self._create_game_objects()
+
+    def _init_game_state(self):
+        """Initialise game state variables (only a small timer for shoot cooldown right now)"""
+        self.dt = 0
+
+    def _init_pygame(self):
+        """Initialise pygame, create game clock and screen"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.dt = 0
-        
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Asteroids - Your pal, Rick")
+
+    def _create_sprite_groups(self):
+        """Create sprite groups for game entities, for easy iteration"""
         self.asteroids = pygame.sprite.Group()
         self.shots = pygame.sprite.Group()
         self.updatable = pygame.sprite.Group()
         self.drawable = pygame.sprite.Group()
 
+    def _assign_containers(self):
+        """Assign sprite classes to groups"""
         Player.containers = (self.updatable, self.drawable)
         Asteroid.containers = (self.updatable, self.drawable, self.asteroids)
         AsteroidField.containers = (self.updatable)
         Shot.containers = (self.shots, self.updatable, self.drawable)
 
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    def _create_game_objects(self):
+        """Create player ship and asteroid field instances"""
         self.my_player = Player(PLAYER_SPAWN_POSITION[0], PLAYER_SPAWN_POSITION[1])
         self.my_asteroidfield = AsteroidField()
 
--- player.py
+++ player.py
@@ -1,6 +1,12 @@
-from circleshape import *
+"""
+Asteroids Game - Player module
+
+And an explanation of it too.
+"""
+
+# Local application imports
 from constants import *
-from shot import *
+from circleshape import * # Import all so it takes the pygame import too. But check whether that's good practise and update accordingly
 
 # Player class 
 class Player(CircleShape):
@@ -48,3 +54,13 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
